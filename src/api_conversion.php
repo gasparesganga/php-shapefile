3dd3bc9a59d01dc179e0d6774d37c0c2ec97dab3
Nick Stallman <nick@agentpoint.com>
2016-09-27 11:22:55 +1000

    Require phpxbase via composer Update phpxbase calls to work with latest version Adjust dbase_functions and api_conversion to not use =&

--- src/api_conversion.php
+++ src/api_conversion.php
@@ -19,14 +19,14 @@
 *
 **/
 
-require_once "Column.class.php";
-require_once "Record.class.php";
-require_once "Table.class.php";
-require_once "WritableTable.class.php";
+use XBase\Column;
+use XBase\Record;
+use XBase\Table;
+use XBase\WritableTable;
 
 function xbase_add_record($xbase_identifier=false,$record) { // - Add a record (array of values) to a dBase database
-	if (!($xbase=&xbase_getInstance($xbase_identifier))) return false;
-	$r =& $xbase->appendRecord();
+	if (!($xbase=xbase_getInstance($xbase_identifier))) return false;
+	$r = $xbase->appendRecord();
 	foreach ($record as $i=>$v) {
 		if (is_object($i))
 			$r->setString($i,$v);
@@ -38,20 +38,20 @@ function xbase_add_record($xbase_identifier=false,$record) { // - Add a record (
 	$xbase->writeRecord();
 }
 function xbase_close($xbase_identifier=false) { // - Close a dBase database
-	if (!($xbase=&xbase_getInstance($xbase_identifier))) return false;
+	if (!($xbase=xbase_getInstance($xbase_identifier))) return false;
 	$xbase->close();
 }
 function xbase_create($filename,$fields) { // - Creates a dBase database
-	if ($xbase =& XBaseWritableTable::create($filename,$fields)) return xbase_addInstance($xbase);
+	if ($xbase = XBaseWritableTable::create($filename,$fields)) return xbase_addInstance($xbase);
 	return false;
 }
 function xbase_delete_record($xbase_identifier=false,$record) { // - Deletes a record from a dBase database
-	if (!($xbase=&xbase_getInstance($xbase_identifier))) return false;
+	if (!($xbase=xbase_getInstance($xbase_identifier))) return false;
 	$xbase->moveTo($record-1);
 	$xbase->deleteRecord();
 }
 function xbase_get_header_info($xbase_identifier=false) { // - Get the header info of a dBase database
-	if (!($xbase=&xbase_getInstance($xbase_identifier))) return false;
+	if (!($xbase=xbase_getInstance($xbase_identifier))) return false;
 	$result = array();
 	foreach ($xbase->columns as $column) {
 		$result[] = array(
@@ -66,18 +66,18 @@ function xbase_get_header_info($xbase_identifier=false) { // - Get the header in
 	return $result;
 }
 function xbase_get_record_with_names($xbase_identifier=false,$record) { // - Gets a record from a dBase database as an associative array 
-	if (!($xbase=&xbase_getInstance($xbase_identifier))) return false;
-	$r =& $xbase->moveTo($record-1);
+	if (!($xbase=xbase_getInstance($xbase_identifier))) return false;
+	$r = $xbase->moveTo($record-1);
 	$result = array();
 	foreach ($xbase->columns as $column) {
-		$result[$column->name] = $r->getString($column);
+		$result[$column->name] = $r->getString($column->name);
 	}
 	$result["deleted"] = $r->isDeleted();
 	return $result;								// A tiny bugfix here, since in the original version the second argument "$record" was missing
 }												//	V
 function xbase_get_record($xbase_identifier=false,$record) { // - Gets a record from a dBase database
-	if (!($xbase=&xbase_getInstance($xbase_identifier))) return false;
-	$r =& $xbase->moveTo($record-1);
+	if (!($xbase=xbase_getInstance($xbase_identifier))) return false;
+	$r = $xbase->moveTo($record-1);
 	$result = array();
 	foreach ($xbase->columns as $column) {
 		$result[] = $r->getString($column);
@@ -86,30 +86,28 @@ function xbase_get_record($xbase_identifier=false,$record) { // - Gets a record 
 	return $result;
 }
 function xbase_numfields($xbase_identifier=false) { // - Find out how many fields are in a dBase database 
-	if (!($xbase=&xbase_getInstance($xbase_identifier))) return false;
+	if (!($xbase=xbase_getInstance($xbase_identifier))) return false;
 	return $xbase->getColumnCount();
 }
 function xbase_numrecords($xbase_identifier=false) { // - Find out how many records are in a dBase database 
-	if (!($xbase=&xbase_getInstance($xbase_identifier))) return false;
+	if (!($xbase=xbase_getInstance($xbase_identifier))) return false;
 	return $xbase->getRecordCount();
 }
 function xbase_open($filename,$flags=0) { // - Opens a dBase database - flags : Typically 0 means read-only, 1 means write-only, and 2 means read and write
 	if ($flags==0) {
-		$xbase =& new XBaseTable($filename);
-		if (!$xbase->open()) return false;
+		$xbase = new Table($filename);
 	} else {
-		$xbase =& new XBaseWritableTable($filename);
-		if (!$xbase->openWrite()) return false;
+		$xbase = new WritableTable($filename);
 	}
 	return xbase_addInstance($xbase);
 }
 function xbase_pack($xbase_identifier=false) { // - Packs a dBase database
-	if (!($xbase=&xbase_getInstance($xbase_identifier))) return false;
+	if (!($xbase=xbase_getInstance($xbase_identifier))) return false;
 	$xbase->pack();
 }
 function xbase_replace_record($xbase_identifier=false,$record,$record_number) { // - Replace a record in a dBase database
-	if (!($xbase=&xbase_getInstance($xbase_identifier))) return false;
-	$r =& $xbase->moveTo($record_number-1);
+	if (!($xbase=xbase_getInstance($xbase_identifier))) return false;
+	$r = $xbase->moveTo($record_number-1);
 	foreach ($record as $i=>$v) {
 		if (is_object($i))
 			$r->setString($i,$v);
@@ -129,17 +127,17 @@ function &xbase_getInstance($i=NULL) {
 	global $xbase_instances;
 	if (sizeof($xbase_instances)==0) trigger_error ("No xbases available", E_USER_ERROR);
 	if (is_null($i)) {
-		$result =& current($xbase_instances);
+		$result = current($xbase_instances);
 	} else {
 		if (!@$xbase_instances[$i]) trigger_error ($i." is an invalid xbase identifier", E_USER_ERROR);
-		$result =& $xbase_instances[$i];
+		$result = $xbase_instances[$i];
 	}
 	return $result;
 }
 function xbase_addInstance(&$i) {
 	global $xbase_instances;
 	$result = sizeof($xbase_instances);
-	$xbase_instances[$result]=&$i;
+	$xbase_instances[$result]=$i;
 	return $result;
 }
 
