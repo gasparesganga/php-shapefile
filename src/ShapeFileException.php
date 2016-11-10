3457217e3f1f96d669157d6df05160fd0918b8a9
Gaspare Sganga <mail@gasparesganga.com>
2016-11-10 16:04:12 +0100

    v2.0.1 release

--- src/ShapeFileException.php
+++ src/ShapeFileException.php
@@ -2,7 +2,7 @@
 /***************************************************************************************************
 ShapeFile - PHP library to read any ESRI Shapefile and its associated DBF into a PHP Array or WKT
     Author          : Gaspare Sganga
-    Version         : 2.0.0
+    Version         : 2.0.1
     License         : MIT
     Documentation   : http://gasparesganga.com/labs/php-shapefile
 ****************************************************************************************************/
